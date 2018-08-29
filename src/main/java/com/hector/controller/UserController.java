9e9ceb90c251ece52c829caad99f8332118edbec
liuwei <wei.liu@kiwinano.com>
2018-08-29 17:13:38 +0800

    *) wip

--- src/main/java/com/hector/controller/UserController.java
+++ src/main/java/com/hector/controller/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -16,10 +18,20 @@ public class UserController {
     @Autowired
     UserMapper userMapper;
 
-    @GetMapping("/users")
-    public String index(Model model){
-        List<User> users = userMapper.selectByExample(null);
-        model.addAttribute("users", users);
-        return "index";
+    @GetMapping("users")
+    public String index() {
+        return "user_list";
+    }
+
+    @ResponseBody
+    @GetMapping("/api/users")
+    public List<User> allUsers() {
+        return userMapper.selectByExample(null);
+    }
+
+    @ResponseBody
+    @GetMapping("/api/users/{id}")
+    public User show(@PathVariable("id") Integer id) {
+        return userMapper.selectByPrimaryKey(id);
     }
 }
